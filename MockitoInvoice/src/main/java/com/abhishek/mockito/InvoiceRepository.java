833f28a2276a996e09513ec5955b9f338320dd80
Abhisd <abhishek.dawange@gmail.com>
2020-08-07 13:38:16 +0530

    Unit testing of invoice

---
+++ MockitoInvoice/src/main/java/com/abhishek/mockito/InvoiceRepository.java
@@ -0,0 +1,10 @@
+package com.abhishek.mockito;
+
+import java.util.Collections;
+import java.util.List;
+
+public class InvoiceRepository {
+	public List<Invoice> all() {
+		return Collections.emptyList();
+	}
+}
