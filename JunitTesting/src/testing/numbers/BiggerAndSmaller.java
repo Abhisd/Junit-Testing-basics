2a71af28e7fb9f562de7b674dd9f878b7c55503b
Abhisd <abhishek.dawange@gmail.com>
2020-08-10 19:02:25 +0530

    checking bigger and smaller number from array

---
+++ JunitTesting/src/testing/numbers/BiggerAndSmaller.java
@@ -0,0 +1,21 @@
+package testing.numbers;
+
+public class BiggerAndSmaller {
+
+	private int bigger=Integer.MIN_VALUE;
+	private int smaller=Integer.MAX_VALUE;
+	public void find(int[] numbers) {
+		for(int n:numbers) {
+			if(n>bigger)bigger=n;
+			if(n<smaller)smaller=n;
+		}
+	}
+	
+	public int getBigger() {
+		return bigger;
+	}
+	public int getSmaller() {
+		return smaller;
+	}
+
+}
